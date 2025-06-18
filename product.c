07967154aaff3248be1f88c64d9a633e71197bac
Yellowfunnah <afnansaleh889@gmail.com>
2025-06-18 18:43:09 +0800

    see the light in the skyyyyyy

---
+++ product.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "product.h"
+
+#define FILENAME "products.txt"
+
+void add_product(){
+
+    FILE *fp;
+    product new_product;
+    fp = fopen(products.txt, "a");
+    if (fp ==NULL){
+        printf("Oops! Error opening file!\n");
+        return;
+    }
+    printf("\n~~~~Add New Product~~~~\n");
+    printf("Enter Product ID: ");
+    Scanf("%s". new_product.productID);
+    while (getchar() != '\n');
+
+    printf("Enter Product Name: ");
+    fgets(new_product.product_name, MAX_NAME_LENGTH, stdin);
+    new_product.product_name[strcspn(new_Product.product_name, "\n")]=0
+
+    printf("Enter Price: ");
+    scanf("%lf", &new_product.price);
+    while (getchar() != '\n');
+
+}
---
+++ product.h
@@ -0,0 +1,13 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+Struct product{
+
+};
+
+void add_product();
+void update_product();
+void delete_product();
+void view_product();
+
+#endif
